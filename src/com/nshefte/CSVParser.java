27cbf31d49eb7631ab6dfc8abc98dd89c849fa45
Nicholas Shefte <shefnic@gmail.com>
2017-04-18 18:46:02 -0500

    Updated comments

--- src/com/nshefte/CSVParser.java
+++ src/com/nshefte/CSVParser.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package com.nshefte;
 
@@ -13,8 +8,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Input a CSV 'text' file and return a 2-day array
- * Allow for column and row headers
+ * Input a CSV 'text' file and return a 2-dimensional array
  * 
  * @author Nicholas
  */
